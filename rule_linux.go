5d76abd97cf73b7c663a7ba4170df65823e26980
世界 <jan@miao.company>
2026-09-19 08:05:36 +0800

    Fix FRA_IP_PROTO attribute size

--- rule_linux.go
+++ rule_linux.go
@@ -155,9 +155,7 @@ func ruleHandle(rule *Rule, req *nl.NetlinkRequest) error {
 	}
 
 	if rule.IPProto > 0 {
-		b := make([]byte, 4)
-		native.PutUint32(b, uint32(rule.IPProto))
-		req.AddData(nl.NewRtAttr(nl.FRA_IP_PROTO, b))
+		req.AddData(nl.NewRtAttr(nl.FRA_IP_PROTO, []byte{uint8(rule.IPProto)}))
 	}
 
 	if rule.Dport != nil {
@@ -266,7 +264,7 @@ func (h *Handle) RuleListFiltered(family int, filter *Rule, filterMask uint64) (
 			case nl.FRA_PRIORITY:
 				rule.Priority = int(native.Uint32(attrs[j].Value[0:4]))
 			case nl.FRA_IP_PROTO:
-				rule.IPProto = int(native.Uint32(attrs[j].Value[0:4]))
+				rule.IPProto = int(attrs[j].Value[0])
 			case nl.FRA_DPORT_RANGE:
 				rule.Dport = NewRulePortRange(native.Uint16(attrs[j].Value[0:2]), native.Uint16(attrs[j].Value[2:4]))
 			case nl.FRA_SPORT_RANGE:
